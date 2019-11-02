97c972142c7594a5a3de62a1ee442669fe27f2d3
hmsk <k.hamasaki@gmail.com>
2019-11-02 00:16:01 -0700

    Reduce portability to focus only adding before file-loader

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-// Really based on react-app-rewire-yaml
-
+// The logic to add loader before 'file-loader' is based on react-app-rewire-yaml: https://github.com/hsz/react-app-rewire-yaml
 const path = require('path');
+
+const fileLoaderMatcher = rule => rule.loader && rule.loader.indexOf(`${path.sep}file-loader${path.sep}`) !== -1;
 
 const ruleChildren = loader => loader.use || loader.oneOf || (Array.isArray(loader.loader) && loader.loader) || [];
 
-const findIndexAndRules = (rulesSource, ruleMatcher) => {
+const findIndexAndRules = (currentRules) => {
   let result = undefined;
-  const rules = Array.isArray(rulesSource) ? rulesSource : ruleChildren(rulesSource);
+  const rules = Array.isArray(currentRules) ? currentRules : ruleChildren(currentRules);
   rules.some(
     (rule, index) =>
-      (result = ruleMatcher(rule)
+      (result = fileLoaderMatcher(rule)
         ? {
             index,
             rules,
           }
-        : findIndexAndRules(ruleChildren(rule), ruleMatcher)),
+        : findIndexAndRules(ruleChildren(rule))),
   );
   return result;
 };
 
-const addBeforeRule = (rulesSource, ruleMatcher, value) => {
-  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher);
-  rules.splice(index, 0, value);
+const addLoaderBeforeFileLoader = (currentRules, newRule) => {
+  const { index, rules } = findIndexAndRules(currentRules);
+  rules.splice(index, 0, newRule);
 };
 
 function rewireFrontMatterMarkdown(config) {
-  const createLoaderMatcher = loader => rule => rule.loader && rule.loader.indexOf(`${path.sep}${loader}${path.sep}`) !== -1;
-  const fileLoaderMatcher = createLoaderMatcher('file-loader');
-  const loader = {
+  const frontmatterMarkdownLoaderRule = {
     test: /\.md$/,
     loader: 'frontmatter-markdown-loader',
     options: {
@@ -35,8 +34,8 @@ function rewireFrontMatterMarkdown(config) {
     }
   };
 
-  addBeforeRule(config.module.rules, fileLoaderMatcher, loader);
+  addLoaderBeforeFileLoader(config.module.rules, frontmatterMarkdownLoaderRule);
   return config;
 }
 
-module.exports = rewireFrontMatterMarkdown
+module.exports = rewireFrontMatterMarkdown;
